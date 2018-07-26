6a9fdb8654c8ee2a97f3dbf2d08e9ed550eb47fd
Ariel Zhen <zhenpingping@live.com>
2018-07-26 23:00:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -21,13 +21,13 @@ class Application
         @@cart.each do |cart_item|
         resp.write "#{cart_item}\n"
         end
-      end 
+      end
     elsif req.path.match(/add/)
-      item_to_add = req.params["item"] 
+      item_to_add = req.params["item"]
       if @@items.include? item_to_add
         @@cart << item_to_add
         resp.write "added #{item_to_add}"
-      else 
+      else
         resp.write "We don't have that item!"
       end 
     else
